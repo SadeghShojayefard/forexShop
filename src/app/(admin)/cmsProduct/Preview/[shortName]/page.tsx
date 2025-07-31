81b5ef82e8a76659c16f3e85e8c2751943e2d3d1
SadeghShojayefard <sadegh.shojayefar@gmail.com>
2025-07-31 08:28:11 +0330

    Update page.tsx

--- src/app/(admin)/cmsProduct/Preview/[shortName]/page.tsx
+++ src/app/(admin)/cmsProduct/Preview/[shortName]/page.tsx
@@ -3,33 +3,46 @@ import DetailSection from "@/components/site/productDetail/detailSection/DetailS
 import { getOneProduct } from "@/helper/productAction";
 import { getTranslations } from "@/i18n/getTranslations";
 
+// تعریف تایپ برای props
+type PageProps = {
+  params: { shortName: string };
+};
 
+// تابع صفحه
+const CmsProductPreviewPage = async ({ params }: PageProps) => {
+  const { shortName } = params; // مستقیم از params می‌گیرم، بدون await
 
-export default async function CmsProductPreviewPage({ params }: { params: { shortName: string } }) {
+  const t = await getTranslations("fa");
+  const productData = await getOneProduct(shortName);
+  const pData = productData?.payload?.data;
+  const slides = pData?.slideImage ?? []; // برای جلوگیری از خطای undefined
+  slides.push(pData?.mainImage);
+  slides.reverse();
 
+  return (
+    <div
+      className="w-full h-full flex flex-col justify-start items-center gap-2 py-5 
+         shadow-2xl shadow-black rounded-2xl px-2"
+    >
+      <b className="text-2xl font-bold w-full border-b pb-2">پیش نمایش محصول</b>
+      <DetailSection
+        productName={pData?.name}
+        user={pData?.userCount}
+        star={pData?.score}
+        price={pData?.finalPriceToman}
+        href={pData?.indicatorFile}
+        img={slides}
+        locale={"fa"}
+        discount={pData?.discount}
+      />
+      <DefinitionSection
+        title={t("definition")}
+        bit64={t("64bit")}
+        text={pData?.textFA}
+        locale="fa"
+      />
+    </div>
+  );
+};
 
-    const resolvedParams = await params;
-    const { shortName } = resolvedParams;
-    
-    const t = await getTranslations("fa")
-    const productData = await getOneProduct(shortName);
-    const pData = productData?.payload?.data;
-    const slides = pData?.slideImage;
-    slides.push(pData?.mainImage);
-    slides.reverse();
-
-    return (
-        <div className="w-full h-full flex flex-col justify-start items-center gap-2   py-5 
-           shadow-2xl shadow-black rounded-2xl px-2 ">
-            <b className="text-2xl font-bold w-full border-b pb-2">پیش نمایش محصول</b>
-            <DetailSection productName={pData?.name} user={pData?.userCount} star={pData?.score}
-                price={pData?.finalPriceToman}
-                href={pData?.indicatorFile} img={slides} locale={"fa"}
-                discount={pData?.discount}
-            />
-
-            <DefinitionSection title={t("definition")} bit64={t("64bit")}
-                text={pData?.textFA} locale="fa" />
-        </div>
-    );
-}
+export default CmsProductPreviewPage;
