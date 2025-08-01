2361956a6fc81f83bd88c66e2e08a32c22eaec04
SadeghShojayefard <sadegh.shojayefar@gmail.com>
2025-08-01 12:01:06 +0330

    Update page.tsx

--- src/app/(site)/[locale]/account/newTicket/[username]/page.tsx
+++ src/app/(site)/[locale]/account/newTicket/[username]/page.tsx
@@ -7,7 +7,8 @@ import { getTranslations } from "@/i18n/getTranslations";
 
 
 export default async function NewTicketsPage({ params }: { params:  Promise< { locale: string; username: string }> }) {
-    const t = await getTranslations(params.locale);
+    const {locale,username} = await params;
+    const t = await getTranslations(locale);
 
     const translation = {
         formTicketTitle: t("formTicketTitle"),
@@ -29,13 +30,13 @@ export default async function NewTicketsPage({ params }: { params:  Promise< { l
         <div className="w-full  flex flex-col justify-center items-center gap-2  text-start  pb-5 shadow-2xl shadow-black rounded-2xl px-2 ">
             <div className="w-full flex flex-row rounded-xl border justify-between items-center gap-2 p-2 py-5 shadow shadow-black mt-5 ">
                 <b>{t("sendNewTicket")}</b>
-                <Link href={`/${params.locale}/account/tickets/sadegh`}
+                <Link href={`/${locale}/account/tickets/sadegh`}
                     className="flex flex-row flex-wrap justify-center items-center gap-2 border p-1 rounded-xl shadow shadow-black
                     hover:bg-sky-300">{t("back")}
                     <IoReturnUpBack color="blue" size="30" />
                 </Link>
             </div>
-            <NewTicket translate={translation} username={params.username} valTranslate={ticketFormVal} locale={params.locale} />
+            <NewTicket translate={translation} username={username} valTranslate={ticketFormVal} locale={params.locale} />
         </div>
 
     );
